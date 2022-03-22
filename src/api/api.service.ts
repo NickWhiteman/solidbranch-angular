9084e66199e8d3642a3511bce251dfc69afbeee3
NickWhiteman <66336085+NickWhiteman@users.noreply.github.com>
2022-03-22 15:28:46 +0300

    update service delete options

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -8,20 +8,13 @@ import { ResponseQuery } from "../type/type";
 })
 export class ApiService {
   private _url: string = 'http://localhost:3000';
-  private _options = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': this._url
-    },
-  }
 
   constructor(private http: HttpClient) {
   };
 
   getData(): Observable<ResponseQuery> {
     return this.http.get<ResponseQuery>(
-      `${this._url}/getData`,
-      this._options
+      `${this._url}/getData`
     );
   };
 } 
